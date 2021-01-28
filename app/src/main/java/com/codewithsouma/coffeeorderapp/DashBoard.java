f5da076c99eadc4c98dc9c800fb20f3f16b5cf63
Souma <souma.hitech@gmail.com>
2021-01-29 02:25:32 +0530

    add listener in first card

--- app/src/main/java/com/codewithsouma/coffeeorderapp/DashBoard.java
+++ app/src/main/java/com/codewithsouma/coffeeorderapp/DashBoard.java
@@ -2,22 +2,61 @@ package com.codewithsouma.coffeeorderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
 //    private int quantity = 2;
+    private final int[] coffeeImages = {
+            R.drawable.americano,
+            R.drawable.black_coffee,
+            R.drawable.cappuccino,
+            R.drawable.mocha,
+            R.drawable.latte,
+            R.drawable.espresso };
+
+    private String[] coffeeNames;
+    private String[] coffeeDescriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        coffeeNames = getResources().getStringArray(R.array.coffee_names);
+        coffeeDescriptions = getResources().getStringArray(R.array.coffee_description);
+
 //        displayQuantity();
 //        displayPrice();
     }
 
     public void handleClick(View view) {
+        int cardIndex = 0;
 
+        if (view == findViewById(R.id.americano_card_ID))
+            cardIndex = 0;
+
+
+        collectDataAndStartNextActivity(cardIndex);
+
+
+    }
+
+    private void collectDataAndStartNextActivity(int cardIndex) {
+        String coffeeName = coffeeNames[cardIndex];
+        String coffeeDescription = coffeeDescriptions[cardIndex];
+        int coffeeImage = coffeeImages[cardIndex];
+
+        startOrderSummaryActivity(coffeeName, coffeeDescription, coffeeImage);
+    }
+
+    private void startOrderSummaryActivity(String coffeeName, String coffeeDescription, int coffeeImage) {
+        Intent orderSummaryActivity = new Intent(this, OrderSummary.class);
+        orderSummaryActivity.putExtra("coffeeName",coffeeName);
+        orderSummaryActivity.putExtra("coffeeImage",coffeeImage);
+        orderSummaryActivity.putExtra("coffeeDescription",coffeeDescription);
+        this.startActivity(orderSummaryActivity);
     }
 
    /* public void submitOrder(View view) {
--- app/src/main/java/com/codewithsouma/coffeeorderapp/OrderSummary.java
+++ app/src/main/java/com/codewithsouma/coffeeorderapp/OrderSummary.java
@@ -3,12 +3,48 @@ package com.codewithsouma.coffeeorderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class OrderSummary extends AppCompatActivity {
+    private int coffeeImage;
+    private String coffeeName;
+    private String coffeeDescription;
+
+    private ImageView coffeeImageView;
+    private TextView coffeeNameTextView;
+    private TextView coffeeDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_summary);
+
+        findAllViews();
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            collectDataFromIntent(bundle);
+            updateUI();
+        }
+
+    }
+
+    private void findAllViews() {
+        coffeeImageView = this.findViewById(R.id.coffee_image_view_ID);
+        coffeeNameTextView = this.findViewById(R.id.coffee_name_text_view_ID);
+        coffeeDescriptionTextView = this.findViewById(R.id.coffee_description_text_view_ID);
+    }
+
+    private void updateUI() {
+        coffeeImageView.setImageResource(coffeeImage);
+        coffeeNameTextView.setText(coffeeName);
+        coffeeDescriptionTextView.setText(coffeeDescription);
+    }
+
+    private void collectDataFromIntent(Bundle bundle) {
+        coffeeName = bundle.getString("coffeeName");
+        coffeeDescription = bundle.getString("coffeeDescription");
+        coffeeImage = bundle.getInt("coffeeImage");
     }
 }
