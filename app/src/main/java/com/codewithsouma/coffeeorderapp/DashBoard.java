83d762a8584512b44baeacd22afd740e508e783c
Souma <souma.hitech@gmail.com>
2021-01-29 02:40:35 +0530

    add listener in all card

--- app/src/main/java/com/codewithsouma/coffeeorderapp/DashBoard.java
+++ app/src/main/java/com/codewithsouma/coffeeorderapp/DashBoard.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
-//    private int quantity = 2;
     private final int[] coffeeImages = {
             R.drawable.americano,
             R.drawable.black_coffee,
@@ -27,19 +26,25 @@ public class DashBoard extends AppCompatActivity {
         coffeeNames = getResources().getStringArray(R.array.coffee_names);
         coffeeDescriptions = getResources().getStringArray(R.array.coffee_description);
 
-//        displayQuantity();
-//        displayPrice();
     }
 
     public void handleClick(View view) {
-        int cardIndex = 0;
+        int cardIndex = -1;
 
         if (view == findViewById(R.id.americano_card_ID))
             cardIndex = 0;
-
+        else if (view == findViewById(R.id.black_coffee_card_ID))
+            cardIndex = 1;
+        else if (view == findViewById(R.id.cappuccino_card_ID))
+            cardIndex = 2;
+        else if (view == findViewById(R.id.mocha_card_ID))
+            cardIndex = 3;
+        else if (view == findViewById(R.id.latte_card_ID))
+            cardIndex = 4;
+        else if (view == findViewById(R.id.espresso_card_ID))
+            cardIndex = 5;
 
         collectDataAndStartNextActivity(cardIndex);
-
 
     }
 
@@ -59,51 +64,4 @@ public class DashBoard extends AppCompatActivity {
         this.startActivity(orderSummaryActivity);
     }
 
-   /* public void submitOrder(View view) {
-        displayQuantity();
-        displayPrice();
-        displayOrderSummary("Souma");
-        startActivity(new Intent(this,OrderSummary.class));
-    }*/
-
-    /*private int calculatePrice() {
-        final int PRICE_OF_EVERY_CUP = 5;
-        return quantity * PRICE_OF_EVERY_CUP;
-    }
-
-    private void displayQuantity() {
-        TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(Integer.toString(quantity));
-    }
-
-    private void displayPrice() {
-        int price = calculatePrice();
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        String formattedPrice = NumberFormat.getCurrencyInstance(Locale.US).format(price);
-        priceTextView.setText("Total price: " + formattedPrice);
-    }*/
-
-    /*public void displayOrderSummary(String name) {
-        int price = calculatePrice();
-        TextView orderSummaryTextView = findViewById(R.id.order_summary_textView);
-        String orderSummary = "Name: " + name + " !!!\n" +
-                "Quantity: " + quantity + " cup of coffee " +
-                "\nTotal price: $" + price + " \n" +
-                "Thank you!!!";
-        orderSummaryTextView.setText(orderSummary);
-
-    }*/
-
-/*    public void increment(View view) {
-        quantity++;
-        displayQuantity();
-        displayPrice();
-    }
-
-    public void decrement(View view) {
-        if (quantity <= 1) return;
-        quantity--;
-        displayQuantity();
-        displayPrice();
-    }*/
 }
--- app/src/main/java/com/codewithsouma/coffeeorderapp/OrderSummary.java
+++ app/src/main/java/com/codewithsouma/coffeeorderapp/OrderSummary.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OrderSummary extends AppCompatActivity {
+    //    private int quantity = 2;
+
     private int coffeeImage;
     private String coffeeName;
     private String coffeeDescription;
@@ -28,6 +30,9 @@ public class OrderSummary extends AppCompatActivity {
             updateUI();
         }
 
+//        displayQuantity();
+//        displayPrice();
+
     }
 
     private void findAllViews() {
@@ -47,4 +52,52 @@ public class OrderSummary extends AppCompatActivity {
         coffeeDescription = bundle.getString("coffeeDescription");
         coffeeImage = bundle.getInt("coffeeImage");
     }
+
+     /* public void submitOrder(View view) {
+        displayQuantity();
+        displayPrice();
+        displayOrderSummary("Souma");
+        startActivity(new Intent(this,OrderSummary.class));
+    }*/
+
+    /*private int calculatePrice() {
+        final int PRICE_OF_EVERY_CUP = 5;
+        return quantity * PRICE_OF_EVERY_CUP;
+    }
+
+    private void displayQuantity() {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(Integer.toString(quantity));
+    }
+
+    private void displayPrice() {
+        int price = calculatePrice();
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        String formattedPrice = NumberFormat.getCurrencyInstance(Locale.US).format(price);
+        priceTextView.setText("Total price: " + formattedPrice);
+    }*/
+
+    /*public void displayOrderSummary(String name) {
+        int price = calculatePrice();
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_textView);
+        String orderSummary = "Name: " + name + " !!!\n" +
+                "Quantity: " + quantity + " cup of coffee " +
+                "\nTotal price: $" + price + " \n" +
+                "Thank you!!!";
+        orderSummaryTextView.setText(orderSummary);
+
+    }*/
+
+/*    public void increment(View view) {
+        quantity++;
+        displayQuantity();
+        displayPrice();
+    }
+
+    public void decrement(View view) {
+        if (quantity <= 1) return;
+        quantity--;
+        displayQuantity();
+        displayPrice();
+    }*/
 }
